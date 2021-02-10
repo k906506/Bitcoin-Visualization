9045702599efb95f09fe20d7ba08bbed115b4dec
k906506 <k906506@naver.com>
2021-02-10 16:54:48 +0900

    korean으로 겹치는거 해결 필요

--- sqllite/practice1.py
+++ sqllite/practice1.py
@@ -1,4 +1,8 @@
 import sqlite3
+import networkx as nx
+import matplotlib.pyplot as plt
+
+dirGraph = nx.DiGraph() # 무방향
 
 conn = sqlite3.connect("dbv-service.db") # dbv3-service.db.와 연결
 
@@ -8,6 +12,18 @@ cur.execute("SELECT * FROM STUDENT") # SELECT 쿼리문 실행
 
 rows = cur.fetchall() # 결과를 rows에 저장
 for row in rows:
-    print("학번 : %s, 이름 : %s, 나이 : %d" %(row[0], row[1], row[2]))
+    name = row[0]
+    subject = row[1]
+    score = row[2]
+    dirGraph.add_nodes_from(row)
+    dirGraph.add_edge(row[0], row[1])
+    dirGraph.add_edge(row[1], row[2])
 
 conn.close() # 연결 해제
+
+nx.draw(dirGraph, with_labels=True)
+
+# 이 py 파일 위치하는 곳에 사진으로 저장
+plt.savefig("practice1.png")
+
+plt.show()
