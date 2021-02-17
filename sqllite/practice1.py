432f035d7ed1ee1d7925ea6e3673b296f0b966db
k906506 <k906506@naver.com>
2021-02-17 18:58:22 +0900

    인덱스 정보, input-output, 실제 해시 저장

--- sqllite/practice1.py
+++ sqllite/practice1.py
@@ -2,28 +2,46 @@ import sqlite3
 import networkx as nx
 import matplotlib.pyplot as plt
 
-dirGraph = nx.DiGraph() # 무방향
+dirGraph = nx.Graph() # 무방향
 
-conn = sqlite3.connect("dbv-service.db") # dbv3-service.db.와 연결
-
+conn = sqlite3.connect("dbv3-core.db") # dbv3-core.db.와 연결
 cur = conn.cursor() # 커서 생성
- 
-cur.execute("SELECT * FROM STUDENT") # SELECT 쿼리문 실행
 
-rows = cur.fetchall() # 결과를 rows에 저장
-for row in rows:
-    name = row[0]
-    subject = row[1]
-    score = row[2]
-    dirGraph.add_nodes_from(row)
-    dirGraph.add_edge(row[0], row[1])
-    dirGraph.add_edge(row[1], row[2])
+cur.execute("SELECT * FROM BlkTx") # SELECT 쿼리문 실행
+blkToTx = cur.fetchall() # 결과를 blkToTx에 저장, blkToTx에는 블록과 트랜잭션 인덱스 사이의 관계가 저장되어있음.
+
+cur.execute("SELECT * FROM txIn") # SELECT 쿼리문 실행
+txInInfo = cur.fetchall() # 결과를 txIn에 저장, txIn에는 tx, n, ptx, pn 순으로 저장.
+
+cur.execute("SELECT * FROM txOut") # SELECT 쿼리문 실행
+txOutInfo = cur.fetchall() # 결과를 txOut에 저장, txOut에는 tx, n, addr, btc 순으로 저장.
+
+index_info = {} # blk와 tx 인덱스 관계, address index, input, output을 저장할 dict형 변수
+
+for row in blkToTx:
+    index_info[row[0]] = [0, 0, 0, 0] # tx 인덱스, address 인덱스, input, output
+    index_info[row[0]][0] = row[1] # tx 인덱스 정보 저장
+
+index = 0
+for row in txOutInfo:
+    index_info[index][1] = row[2] # row[2]는 address 인덱스
+    index_info[index][3] = row[3] # row[3]은 output
+    index += 1
+
+############################################################################################
+
+hash_info = {} # index_info에 들어있는 index의 실제 해시
+
+conn = sqlite3.connect("dbv3-index.db") # dbv3-index.db.와 연결
+cur = conn.cursor() # 커서 생성
+
+cur.execute("SELECT * FROM BlkID") # SELECT 쿼리문 실행
+blkIdInfo = cur.fetchall() # 결과를 blkIdInfo에 저장, blkIdInfo에는 index와 실제 blk 해시 주소가 저장
+
+cur.execute("SELECT * FROM AddrId") # SELECT 쿼리문 실행
+addrIdInfo = cur.fetchall() # 결과를 addrIdInfo 저장, addrIdInfo에는 index와 실제 address 해시 주소가 저장
+
+cur.execute("SELECT * FROM TxID") # SELECT 쿼리문 실행
+txIdInfo = cur.fetchall() # 결과를 txIdInfo 저장, txIdInfo에는 index와 실제 tx 해시 주소가 저장
 
 conn.close() # 연결 해제
-
-nx.draw(dirGraph, with_labels=True)
-
-# 이 py 파일 위치하는 곳에 사진으로 저장
-plt.savefig("practice1.png")
-
-plt.show()
