20f6b48e7b16c0cf50bdc76c91d926044ae3bd30
k906506 <k906506@naver.com>
2021-02-19 12:25:53 +0900

    Addr -> Cluster 그래프 구축

--- sqllite/addrToCluster_dirGraph.py
+++ sqllite/addrToCluster_dirGraph.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 dirGraph = nx.DiGraph() # 방향 그래프
 
 print("탐색을 위한 index의 범위를 입력해주세요. (총 개수, OFFSET)")
-print("[Input] 10 30 -> [Output] 31행 ~ 50행까지의 그래프")
+print("[Input] 10 30 -> [Output] 31행 ~ 40행까지의 그래프")
 
 cnt, offset = map(int, input().split())
 
 conn = sqlite3.connect("dbv3-service.db") # dbv3-core.db.와 연결
 cur = conn.cursor() # 커서 생성
 cur.execute("SELECT * FROM Cluster LIMIT %d OFFSET %d" %(cnt, offset)) # SELECT 쿼리문 실행
-clusterInfo = cur.fetchall() # 결과를 clusterInfo에 저장, clusterInfo에는 blk와 tx 인덱스 관계가 저장.
+clusterInfo = cur.fetchall() # 결과를 clusterInfo에 저장, clusterInfo에는 addr과 cluster의 인덱스 관계가 저장.
 
 # 특정 클러스터와 이와 관련된 addr이 dict형 변수에 저장.
 # node[cluster number] = [cluster number, [addr]]
@@ -30,9 +30,6 @@ for row in clusterInfo:
     node[row[1]][1].append(row[0]) # addr 저장.
 
 # addr -> cluster 방향성을 지닌 그래프 생성
-for element in node:
-    dirGraph.add_node("Cluster " + str(node[element][0]))
-
 for element in node:
     for addr in node[element][1]:
         dirGraph.add_edge("Addr " + str(addr), "Cluster " + str(node[element][0]))
