f8ecdbaa476bc1fa6baaa966ba8057954493db58
k906506 <k906506@naver.com>
2021-03-04 15:56:26 +0900

    작업중

--- sqllite/unionDB.py
+++ sqllite/unionDB.py
@@ -22,24 +22,47 @@ def getAddr(cluster):
     addrInfo = cur.fetchall()
     return addrInfo
 
+def getTx(address):
+    cur.execute("SELECT * FROM TxOut where Txout.addr = %d order by n desc" %address)
+    txInfo = cur.fetchall()
+    return txInfo
+
+def getCntTxAddrBTC(address): # 특정 주소의 주소의 총 사용량과 거래 금액 리턴
+    cur.execute("SELECT TxOut.tx, count(TxIn.n),TxOut.btc From TxIn INNER JOIN TxOut On TxIn.pn = TxOut.n and TxIn.ptx = TxOut.tx where TxOut.addr = %d" %address)
+    cntInfo = cur.fetchall()
+    return cntInfo
+
 def main():
     conn = sqlite3.connect("dbv3-service.db")
     cur = conn.cursor()
 
-    userInput = input().rstrip()
-    tagId = getTagId(userInput)
-    clusterInfo = getCluster(tagId)
+    userInputSrt = input().rstrip()
+    userInputDst = input().rstrip()
+    tagIdSrt = getTagId(userInputSrt)
+    tagIdDst = getTagId(userInputDst)
+    clusterInfoSrt = getCluster(tagIdSrt)
+    clusterInfoDst = getCluster(tagIdDst)
     
-    sumAddrInfo = []
-    for cluster in clusterInfo:
+    sumAddrInfoSrt = []
+    sumAddrInfoDst = []
+    for cluster in clusterInfoSrt:
         addrInfo = getAddr(cluster[0])
-        sumAddrInfo.extend(addrInfo)
-    
+        sumAddrInfoSrt.extend(addrInfo)
+    for cluster in clusterInfoDst:
+        addrInfo = getAddr(cluster[0])
+        sumAddrInfoDst.extend(addrInfo)
+
+    #####################################################################################################
+    # 1. 위 과정이 종료되면 sumAddrInfoSrt에는 거래소 A의 모든 주소가, sumAddrInfoDst에는 거래소 B의 모든 주소가 저장. #
+    #####################################################################################################
+
     cur.close()
     conn.close()
 
     conn = sqlite3.connect("dbv3-core.db")
     cur = conn.cursor()
+
+
 
     
 
