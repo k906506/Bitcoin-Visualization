33fbbcc05ec9ce80542c6578752ae45eb6a89b61
k906506 <k906506@naver.com>
2021-03-10 12:24:50 +0900

    1차 거래(거래소 A -> 거래소 B) 구축

---
+++ sqllite/firstTransaction.py
@@ -0,0 +1,192 @@
+import sqlite3
+import time
+
+def getTagId(userInput): # 입력 값에 따른 tagId 리턴
+    if userInput == "업비트":
+        return 942
+    elif userInput == "빗썸":
+        return 167
+    elif userInput == "코인원":
+        return 390
+    else:
+        return -1
+
+def main():
+    conn = sqlite3.connect("dbv3-service.db")
+    cur = conn.cursor()
+
+    print("거래소 A를 입력해주세요.")
+    srt = input().rstrip()
+    userInputSrt = getTagId(srt)
+
+    print("")
+    print("거래소 B를 입력해주세요.")
+    dst = input().rstrip()
+    userInputDst = getTagId(dst)
+
+    notEqualSrtDst = True
+    if srt == dst:
+        notEqualSrtDst = False
+
+    print("")
+    print("%s와 %s의 모든 주소를 검색합니다." %(srt, dst))
+
+    cur.execute("SELECT Tag.addr FROM Tag where Tag.tag = %d" %userInputSrt)
+    tagIdSrt = cur.fetchall()
+
+    if notEqualSrtDst:
+        cur.execute("SELECT Tag.addr FROM Tag where Tag.tag = %d" %userInputDst)
+        tagIdDst = cur.fetchall()
+    else:
+        tagIdDst = tagIdSrt
+    
+    sumClusterInfoSrt = []
+    sumClusterInfoDst = []
+    sumAddrInfoSrt = []
+    sumAddrInfoDst = []
+    for addr in tagIdSrt:
+        cur.execute("SELECT Cluster.cluster FROM Cluster where Cluster.addr = %d" %addr[0])
+        clusterInfo = cur.fetchall()
+        sumClusterInfoSrt.extend(clusterInfo)
+    for cluster in sumClusterInfoSrt:
+        cur.execute("SELECT Cluster.addr FROM Cluster where Cluster.cluster = %d" %cluster[0])
+        addrInfo = cur.fetchall()
+        sumAddrInfoSrt.extend(addrInfo)
+
+    print("[%s] 총 %d개의 주소가 검색되었습니다." %(srt, len(sumAddrInfoSrt)))
+
+    if notEqualSrtDst:
+        for addr in tagIdDst:
+            cur.execute("SELECT Cluster.cluster FROM Cluster where Cluster.addr = %d" %addr[0])
+            clusterInfo = cur.fetchall()
+            sumClusterInfoDst.extend(clusterInfo)
+        for cluster in sumClusterInfoDst:
+            cur.execute("SELECT Cluster.addr FROM Cluster where Cluster.cluster = %d" %cluster[0])
+            addrInfo = cur.fetchall()
+            sumAddrInfoDst.extend(addrInfo)
+    else:
+        sumClusterInfoDst = sumClusterInfoSrt
+        sumAddrInfoDst = sumAddrInfoSrt
+
+    print("[%s] 총 %d개의 주소가 검색되었습니다." %(dst, len(sumAddrInfoDst)))
+
+    cur.close()
+    conn.close()
+
+    ####################################################################################################
+    #1. 위 과정이 종료되면 sumAddrInfoSrt에는 거래소 A의 모든 주소가, sumAddrInfoDst에는 거래소 B의 모든 주소가 저장. #
+    ####################################################################################################
+
+    print("")
+    print("%s와 %s의 모든 트랜잭션을 검색합니다." %(srt, dst))
+
+    conn = sqlite3.connect("dbv3-core.db")
+    cur = conn.cursor()
+
+    visitSrt = []
+    visitDst = []
+    txInfoSrt = {}
+    txInfoDst = {}
+
+    for addr in sumAddrInfoSrt: # 거래소 A(Out)의 모든 트랜잭션 저장
+        cur.execute("SELECT TxOut.tx FROM TxOut INNER JOIN TxIn on TxOut.tx = TxIn.ptx where TxOut.addr = %d" %addr[0])
+        try:
+            txInfo = cur.fetchone()[0]
+        except:
+            continue
+        if txInfo not in visitSrt:
+            txInfoSrt[txInfo] = []
+            visitSrt.append(txInfo)
+        txInfoSrt[txInfo].append(addr[0])    
+
+    print("[%s]의 총 %s개의 트랜잭션이 검색되었습니다." %(srt, len(txInfoSrt)))
+
+    for addr in sumAddrInfoDst: # 거래소 B(In)의 모든 트랜잭션 저장
+        cur.execute("SELECT TxOut.tx FROM TxOut INNER JOIN TxIn on TxOut.tx = TxIn.tx where TxOut.addr = %d" %addr[0])
+        try:
+            txInfo = cur.fetchone()[0]
+        except:
+            continue
+        if txInfo not in visitDst:
+            txInfoDst[txInfo] = []
+            visitDst.append(txInfo)
+        txInfoDst[txInfo].append(addr[0]) 
+
+    print("[%s]의 총 %s개의 트랜잭션이 검색되었습니다." %(dst, len(txInfoDst)))
+
+    sortTxInfoSrt = sorted(txInfoSrt.items(), key = lambda x : len(x[1]), reverse = True)
+    sortTxInfoDst = sorted(txInfoDst.items(), key = lambda x : len(x[1]), reverse = True)
+
+    print("")
+    print("주소의 개수가 1개인 트랜잭션을 검색합니다.")
+
+    indexSrt = 0
+    indexDst = 0
+
+    for txSrt in sortTxInfoSrt:
+        if len(txSrt[1]) == 1:
+            break
+        indexSrt += 1
+
+    for txDst in sortTxInfoDst:
+        if len(txDst[1]) == 1:
+            break
+        indexDst += 1
+    
+    print("주소의 개수가 1개인 트랜잭션을 제외합니다.")
+    
+    newSortTxInfoSrt = []
+    newSortTxInfoDst = []
+    newSortTxInfoSrt.extend(sortTxInfoSrt[:indexSrt+1])
+    newSortTxInfoDst.extend(sortTxInfoDst[:indexDst+1])
+
+    txInfoSrtToDst = {}
+    visitSrtToDst = []
+
+    print("")
+    print("%s에서 %s으로 이동한 트랜잭션을 검색합니다." %(srt, dst))
+    for txSrt in newSortTxInfoSrt:
+        for txDst in newSortTxInfoSrt:
+            cur.execute("SELECT count(TxOut.addr), sum(TxOut.btc) FROM TxOut INNER JOIN TxIn on TxOut.tx = TxIn.tx where TxIn.ptx = %d and TxOut.tx = %d" %(txSrt[0], txDst[0]))
+            try:
+                txInfo = cur.fetchone()
+            except:
+                continue
+            if txInfo[0] == 0:
+                continue
+            else:
+                if txSrt[0] not in visitSrtToDst:
+                    txInfoSrtToDst[txSrt[0]] = []
+                    visitSrtToDst.append(txSrt[0])
+                txInfoSrtToDst[txSrt[0]].append([txDst[0], txInfo[0], txInfo[1]])
+    
+    print("총 %d개의 트랜잭션이 검색되었습니다." %len(txInfoSrtToDst))
+
+    txInfoSrtToDst = sorted(txInfoSrtToDst.items(), key = lambda x : (x[1][0][1], x[1][0][2]), reverse = True)
+
+    print("")
+    print("(1) 주소의 개수가 많고, (2) 거래 금액이 높은 트랜잭션부터 내림차순으로 정렬합니다.")
+
+    index = 1
+    for element in txInfoSrtToDst:
+        print("[%3d] [TxOut] %9d [TxIn] %9d [Addr Cnt] %3d [BTC] %10.10f" %(index, element[0], element[1][0][0], element[1][0][1], element[1][0][2]))
+        index += 1
+
+    print("")
+    print("탐색할 트랜잭션의 INDEX를 입력해주세요.")
+    index = int(input())
+
+    print("")
+    print("%d번째의 거래를 출력합니다." %index)
+
+    infoSrtToDst = txInfoSrtToDst[index-1]
+    cur.execute("SELECT TxOut.addr, TxOut.btc FROM TxOut INNER JOIN TxIn on TxOut.tx = TxIn.tx where TxIn.tx = %d" %infoSrtToDst[1][0][0])
+    info = cur.fetchall()
+
+    index = 1
+    for element in info:
+        print("[%3d] [TxOut] %9d [addr] %9d [BTC] %10.10f" %(index, infoSrtToDst[0], element[0], element[1]))
+        index += 1
+
+if __name__ == "__main__":
+    main()
