6556eaa46466cf6a66375167797f01d03affc53f
k906506 <k906506@naver.com>
2021-06-06 20:28:04 +0900

    출력문 수정

--- sqllite/visualTransactionForType.py
+++ sqllite/visualTransactionForType.py
@@ -68,16 +68,16 @@ def main():
     ans = sorted(count.items(), key = lambda x : x[1], reverse=True)
 
     print("")
-    print("가중치 입력 : ", end = "")
+    print("Minimum Value 입력 = ", end = "")
     max_node = int(input()) # 가중치
     for i in range(len(ans)): # 가중치까지의 초록색 노드 개수
         if ans[i][1] <= max_node:
             index = i+1
             break
 
-    print("직접 연결노드 총 개수 : %d" %countGreen)
-    print("간접 연결노드 총 개수 : %d" %countBlue)
-    print("[가중치 %d] 1개의 직접 연결노드에 연결된 간접 연결노드 평균 개수 : %f" %(max_node, (countBlue-(countGreen-index))/(countGreen-index)))
+    print("직접 연결노드 총 개수 = %d" %countGreen)
+    print("간접 연결노드 총 개수 = %d" %countBlue)
+    print("[Minimum Value %d] 간접 연결노드 : 직접 연결노드 = %f" %(max_node, (countBlue-(countGreen-index))/(countGreen-index)))
 
     G.show_buttons(filter_=['physics'])
     G.show("visualTransactionForType.html")
